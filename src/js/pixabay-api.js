4695b4723a00e38124c3371a93c65f4e4f384556
Caviar <forworker137@gmail.com>
2024-12-10 01:03:11 +0200

    third variant

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,18 +3,19 @@
 import axios from 'axios';
 
 const API_KEY = '47493519-a1c562378b8b6eff6e1575382';
-axios.defaults.headers.common['key'] = API_KEY;
+// axios.defaults.headers.common['key'] = API_KEY;
 
 export default async function fetchImages(filter, page) {
   return await axios.get('https://pixabay.com/api', {
     params: {
-      // key: API_KEY,
+      key: API_KEY,
       q: filter,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
       page,
       per_page: 15,
+      withCredentials: false,
     },
   });
 }
